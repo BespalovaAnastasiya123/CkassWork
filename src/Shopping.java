295495455a40e79fa39a28216d157fb3b7664a3a
BespalovaAnastasiya123 <3537184@gmail.com>
2024-03-14 20:13:12 +0300

    11

---
+++ src/Shopping.java
@@ -0,0 +1,55 @@
+public class Shopping {
+    //pattern Decorator
+    public static void main(String[] args) {
+        Man man = new Dressmaker(new Shoemaker(Pedestrian()));
+
+    }
+}
+interface Man{
+    void walk();
+}
+
+class Pedestrian implements Man{
+
+    @Override
+    public void walk() {
+        System.out.println("I go down the street");
+    }
+}
+abstract class Decorator implements Man{
+    protected Man man;
+
+    public Decorator(Man man) {
+        this.man = man;
+    }
+    @Override
+    public void walk() {
+        man.walk();
+
+    }
+    class Shoemaker extends Decorator{
+
+        public Shoemaker(Man man) {
+            super(man);
+        }
+
+        @Override
+        public void walk() {
+            System.out.println("I made shoes for  man");
+
+        }
+    }
+
+}
+class Dressmaker extends Decorator{
+
+    public Dressmaker(Man man) {
+        super(man);
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("I made dress for the man");
+
+    }
+}
