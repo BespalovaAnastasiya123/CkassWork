d5e830dd58b814d2c1619d81d9f35aabe0820509
BespalovaAnastasiya123 <3537184@gmail.com>
2024-03-21 21:29:34 +0300

    11

--- src/StreamAPI/IntStreams.java
+++ src/StreamAPI/IntStreams.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.lang.Long.sum;
 
 public class IntStreams {
     public static void main(String[] args) {
@@ -28,26 +31,38 @@ public class IntStreams {
         System.out.println(sArr);
 
         String sArr2 = Arrays.stream(randArr)
-                .filter(i->i%2==0)
+                .filter(i -> i % 2 == 0)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining(","));
         System.out.println(sArr2);
 
-        IntStream.range(1,1001)
-                .filter(i->i%3==0 || i%5==0 || i%11==0)
+        IntStream.range(1, 1001)
+                .filter(i -> i % 3 == 0 || i % 5 == 0 || i % 11 == 0)
                 .forEach(System.out::println);
 
         //вывести квадраты первых n натуральных чисел
-        IntStream.range(1,11)
-                .map(i->i*i)
+        IntStream.range(1, 11)
+                .map(i -> i * i)
                 .forEach(System.out::println);
         //ывести степени числа 2 (первые 10)
-        IntStream.range(1,11)
-                .map(i->(int)Math.pow(2,i))
+        IntStream.range(1, 11)
+                .map(i -> (int) Math.pow(2, i))
                 .forEach(System.out::println);
 
-        IntStream.iterate(2,i->i*2)
+        IntStream.iterate(2, i -> i * 2)
                 .limit(10)
                 .forEach(System.out::println);
+
+        //первое число, сумма которого равна 20
+        Stream.iterate(new int[]{1, 1}, x -> new int[]{++x[0], x[1] = sum(Integer.toString(x[0]).chars().toArray())})//задаем начальное значение
+                .filter(x -> x[1] == 20)
+                .limit(1)
+                .forEach(x -> System.out.println(x[0]));
     }
-}
+        public static int sum(int[] arr) {
+            int result = 0;
+            for (int a : arr) result += a - '0';// char ->int
+            return result;
+        }
+    }
+
